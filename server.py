cf186c228fef3682e8343a1e8b2de77fc0bd4394
Gokul Prasad <gokulprasad497@gmail.com>
2026-06-02 23:59:12 +0530

    Implement basic chat server with threading

---
+++ server.py
@@ -0,0 +1,50 @@
+import socket
+import threading
+
+HOST = '0.0.0.0'
+PORT = 5555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            broadcast(f'{nickname} left the chat!'.encode())
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send('NICK'.encode())
+        nickname = client.recv(1024).decode()
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname: {nickname}")
+        broadcast(f"{nickname} joined the chat!".encode())
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server running...")
+receive()
